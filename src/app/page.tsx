c659dc6981038d8c4228bc4aee1f8334bdfac8f2
SylvHardDev <hardsyldev@gmail.com>
2024-12-18 15:14:01 +0300

    apprendre dialogue component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,30 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 export default function Home() {
   return (
-    <Button variant={"ghost"}>Test</Button>
+    <div className="w-full h-screen flex items-center justify-center">
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button>Open</Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </div>
   );
 }
